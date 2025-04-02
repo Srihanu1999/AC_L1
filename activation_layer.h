5c61680ab133ff8e71d16cc53c970be83183adb2
Dharani_P <144601763+Srihanu1999@users.noreply.github.com>
2025-04-02 20:28:45 +0530

    Create activation_layer.h

---
+++ activation_layer.h
@@ -0,0 +1,8 @@
+#ifndef ACTIVATION_LAYER_H
+#define ACTIVATION_LAYER_H
+
+void apply_leaky_relu(float* input, int size, float alpha);
+void apply_tanh(float* input, int size);
+void apply_sigmoid(float* input, int size);
+
+#endif // ACTIVATION_LAYER_H
